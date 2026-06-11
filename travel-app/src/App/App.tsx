63d5ba5a177a21322c28a059d0897d87c12c336d
deadOk <fomka15012007@yandex.ru>
2026-06-11 16:00:25 +0500

    Организовал рабочее пространство

---
+++ travel-app/src/App/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import '../Styles/index.scss';
+import './Styles.scss';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>asdlfjhal</div>
+    </>
+  )
+}
+
+export default App
